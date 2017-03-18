bd2a2a624b4845398397ee7e1779ed17824d4c51
Vijay Velagapudi <vijayv@fb.com>
2017-03-17 20:18:45 -0700

    Update styling for better alignment

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
 const child = require('child_process');
-const concat = require('gulp-concat');
 const cssnano = require('gulp-cssnano');
 const imagemin = require('gulp-imagemin');
 const gutil = require('gulp-util');
@@ -10,12 +8,11 @@ const sass = require('gulp-sass');
 const siteRoot = '_site';
 
 // Concat all scss files together
-const scssFiles = './_scss/**/*.scss';
+const scssFiles = './_scss/*.scss';
 gulp.task('css', () => {
   gulp.src(scssFiles)
     .pipe(sass())
     .pipe(cssnano())
-    .pipe(concat('all.css'))
     .pipe(gulp.dest('assets'))
 });
 
@@ -29,12 +26,11 @@ gulp.task('images', () =>
 
 gulp.task('watch', () => {
   gulp.watch(scssFiles, ['css']);
-  gulp.watch(imageFiles, ['images']);
 });
 
 // Sets gulp command to build the site
 gulp.task('jekyll', () => {
-  const jekyll = child.spawn('jekyll', ['build',
+  const jekyll = child.spawn('jekyll', ['serve',
     '--watch',
     '--incremental',
     '--drafts'
@@ -50,15 +46,4 @@ gulp.task('jekyll', () => {
   jekyll.stderr.on('data', jekyllLogger);
 });
 
-// Live reloading with browser sync
-gulp.task('serve', () => {
-  browserSync.init({
-    files: [siteRoot + '/**'],
-    port: 4000,
-    server: {
-      baseDir: siteRoot
-    }
-  });
-});
-
-gulp.task('default', ['css', 'images', 'jekyll', 'serve']);
+gulp.task('default', ['css', 'watch', 'jekyll']);
